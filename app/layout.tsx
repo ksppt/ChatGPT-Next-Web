86c14980a4ac548d9bdd205c35bfb0a4f694c5f4
ksppt <127325410+ksppt@users.noreply.github.com>
2023-05-07 20:12:46 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "购买地址https://ksppt.top/",
+  title: "ChatGPT Next Web",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
     title: "ChatGPT Next Web",
